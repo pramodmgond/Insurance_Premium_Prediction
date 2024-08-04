b58f57030938c83d94e92399de6cdc5a0d16fb4b
pramod <pramodgond0075@gmail.com>
2024-08-04 21:06:11 +0530

    data drift

--- Insurance_Prediction/pipeline/training_pipeline.py
+++ Insurance_Prediction/pipeline/training_pipeline.py
@@ -2,9 +2,11 @@ from Insurance_Prediction.exception import InsuranceException
 from Insurance_Prediction.logger import logging
 
 from Insurance_Prediction.components.data_ingestion_old import DataIngestion
+from Insurance_Prediction.components.data_validation import DataValidation
 
-from Insurance_Prediction.entity.config_entity import (DataIngestionConfig)                                   
-from Insurance_Prediction.entity.artifact_entity import (DataIngestionArtifact)
+
+from Insurance_Prediction.entity.config_entity import (DataIngestionConfig, DataValidationConfig)                                   
+from Insurance_Prediction.entity.artifact_entity import (DataIngestionArtifact, DataValidationArtifact)
 
 import os
 import sys 
@@ -12,6 +14,8 @@ import sys
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
+
 
 
 
@@ -35,12 +39,42 @@ class TrainPipeline:
         
     
     
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data validation component
+        """
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=self.data_validation_config
+                                             )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise InsuranceException(e, sys) from e
+        
+
+        
+    
+    
     def run_pipeline(self, ) -> None:
         """
         This method of TrainPipeline class is responsible for running complete pipeline
         """
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+
         
         
         except Exception as e:
